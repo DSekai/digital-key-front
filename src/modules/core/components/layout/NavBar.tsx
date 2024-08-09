005fe462e6ae6bd53edd3d7d8fccfd0a73af56f8
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-08-09 02:19:59 -0400

    feat: new styles and move files

---
+++ src/modules/core/components/layout/NavBar.tsx
@@ -0,0 +1,60 @@
+import './layout.css'
+import Logo from '../../../../assets/icons/logo'
+import { DropDown } from '../../../../assets/icons/DropDown'
+import { Cart } from '../../../../assets/icons/Cart'
+import { ArrowRight, ArrowRightV2 } from '../../../../assets/icons/ArrowRight'
+
+export const Navbar = () => {
+  return (
+    <nav className='navbar'>
+
+      <div className="main">
+        <a className="menu-logo" aria-label='logo' href='https://sekaidev.netlify.app'>
+          <Logo height="100%" width={"100%"} />
+        </a>
+        <div className="options">
+          <div>
+            <p>Store</p>
+            <DropDown width={"1rem"} />
+            <span className='menu-triangle active'></span>
+            <div className="sub-menu">
+              <div className="selection top">
+                <p>Best Games</p>
+                <div>
+                  <ArrowRight className='arrow-simple' />
+                  <ArrowRightV2 className='arrow-double' />
+                </div>
+              </div>
+              <div className="selection">
+                <p>otro ma</p>
+                <div>
+                  <ArrowRight className='arrow-simple' />
+                  <ArrowRightV2 className='arrow-double' />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p>About</p>
+            <DropDown width={"1rem"} />
+            <span className='menu-triangle'></span>
+          </div>
+          <div>
+            <p>Community</p>
+            <DropDown width={"1rem"} />
+          </div>
+          <div className="">
+            <p>Support</p>
+            <DropDown width={"1rem"} />
+          </div>
+          <p>Sign In</p>
+        </div>
+        <a className="cart" aria-label='Cart' href='#'>
+          <Cart />
+          <span>0</span>
+        </a>
+      </div>
+
+    </nav>
+  )
+}
