15fd32bb3c5a10eed48393d709643dd32df79df0
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-08-12 20:57:45 -0400

    new section

---
+++ src/modules/product/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import "./product.css"
+
+export const ProductCard = () => {
+  return (
+    <div className="productCard">
+      <header>
+
+      </header>
+      <div className="body"></div>
+    </div>
+  )
+}
